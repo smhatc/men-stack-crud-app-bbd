4f4e0fbcce8192adf3668bc4f90d9ad37124dfc6
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-07-13 16:03:06 +0300

    Built landing page

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public")));
+
 // ROUTES
 app.get("/", (req, res) => {
-        res.send("Hello, friend.");
+        res.render("index.ejs");
 });
 
 // STARTING THE SERVER
 app.listen(port, (req, res) => {
-        console.log(`Server is listening on port ${port}.`)
+        console.log(`Server is listening on port ${port}.`);
 });
