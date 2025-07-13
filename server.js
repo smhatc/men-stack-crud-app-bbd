eba20f9563e7f9333832f4606f1a6afdbd89ae7a
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-07-13 11:33:31 +0300

    Added index (home) route and installed EJS

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// ROUTES
+app.get("/", (req, res) => {
+        res.send("Hello, friend.");
+});
+
+// STARTING THE SERVER
 app.listen(port, (req, res) => {
         console.log(`Server is listening on port ${port}.`)
 });
